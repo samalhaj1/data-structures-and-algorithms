54772c7fc1585fd522af719de12328dc884f4ace
samalhaj1 <samalhaj627@gmail.com>
2021-08-31 21:40:56 +0300

    add new thing

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -72,7 +72,7 @@ CHALLENGE 5
 
 Write a function named notInFirstArray that, given two arrays as input, uses filter to return an array of all the elements in the second array that are not included in the first array.
 
-For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
+For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4]....
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
